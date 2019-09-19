cc556404e3c42e80c66f4e9b79526506e86adeb5
lizdoyle <lizdoyle1121@gmail.com>
2019-09-18 22:23:33 -0400

    fixed and cmptd MVP

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
-<<<<<<< HEAD
-//Context
-import { ProductContext } from "./contexts/ProductContext";
-import { CartContext } from "./contexts/CartContext";
-
-
-=======
->>>>>>> parent of ea7d9fa... initial push, steps 1-3 cmplt
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
@@ -24,50 +19,26 @@ function App() {
 	};
 
 	return (
-<<<<<<< HEAD
-		
-		<div className="App">
-			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }} >
-					<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+
+			<div className="App">
+			<CartContext.Provider value={{cart}}>
+				<Navigation cart={cart} />
 				</CartContext.Provider>
-			</ProductContext.Provider>
-					{/* Routes */}
-			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={{ cart }} >
-					<Route exact path="/" components={Products} />
+				{/* Routes */}
+				<Route
+					exact
+					path="/" component={Products}
+				/>
+				
+			<CartContext.Provider value={{ cart }}>
+				<Route
+					path="/cart" component={ShoppingCart}
+				/>
 				</CartContext.Provider>
-			</ProductContext.Provider>
+			</div>
+		</ProductContext.Provider >
 
-				<ProductContext.Provider value={{ products, addItem }}>
-					<CartContext.Provider value={{ cart }} >
-						<Route path="/cart" components={ShoppingCart} />
-					</CartContext.Provider>
-				</ProductContext.Provider>
-		</div>
-	
-=======
-		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
->>>>>>> parent of ea7d9fa... initial push, steps 1-3 cmplt
 	);
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,21 @@
-<<<<<<< HEAD
 import React, {useContext} from 'react';
-import { ProductContext } from "../contexts/ProductContext";
 
-=======
-import React from 'react';
->>>>>>> parent of ea7d9fa... initial push, steps 1-3 cmplt
+import {ProductContext} from "../contexts/ProductContext";
 
 // Components
 import Product from './Product';
 
-<<<<<<< HEAD
- 
-
 const Products = () => {
 
-	const { products, addItem } = useContext(ProductContext);
+	const {products, addItem} = useContext(ProductContext);
 
-
-=======
-const Products = props => {
->>>>>>> parent of ea7d9fa... initial push, steps 1-3 cmplt
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+					addItem={addItem}
 				/>
 
 
